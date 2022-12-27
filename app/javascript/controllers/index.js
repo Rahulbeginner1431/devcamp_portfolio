415adad2e4107883043375d2dac64b95dec24baf
rahul <rahulkoli@gmail.com>
2022-12-27 11:34:51 +0530

    changes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,8 +5,11 @@ import { application } from "controllers/application"
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+import eagerLoadControllersFrom for (var i = Things.length - 1; i >= 0; i--) {
+	Things[i]
+}
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
-hello rahul
+
